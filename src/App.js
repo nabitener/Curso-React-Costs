eb7f9fd7dc112843576000f530c6310cd38d1120
Nathalia Bitener <nathaliab@ciandt.com>
2023-01-27 15:53:22 -0300

    Aula 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+import NewProject from "./components/pages/NewProject";
+
+import Container from "./components/layout/Container";
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Contato</Link>
+        <Link to="/contact">Company</Link>
+        <Link to="/newproject">NewProject</Link>
+      </div>
+
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/company" element={<Company />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/newproject" element={<NewProject />}></Route>
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
