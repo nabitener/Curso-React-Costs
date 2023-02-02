dbebfd6ee0bdc7428ef94b98269c99db412184d8
Nathalia Bitener <nathaliab@ciandt.com>
2023-02-02 10:47:19 -0300

    Aula 7: Conectando com a API

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,11 +1,17 @@
 import styles from "./Select.module.css";
 
 function Select({ text, name, options, handleOnChange, value }) {
+  
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}</label>
       <select name={name} id={name}>
-        <option>Selecione uma opção</option>
+        <option> Selecione uma opção</option>
+          {options.map((item) => (
+            <option value={item.id} key={item.id}>
+              {item.name}
+            </option>
+          ))}
       </select>
     </div>
   );
